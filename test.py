46fedc90dde9be9c6c6ea3f87b24eb0ab201b524
LEIA-Tec <LEIA.tecqro@outlook.com>
2025-09-29 10:27:59 -0600

    Update test.py

--- test.py
+++ test.py
@@ -3,32 +3,45 @@ import os
 import sys
 import time
 
-print("gym:", gym.__version__)    # should print 0.21.0
-print("retro:", retro.__version__) # should print 0.8.0 (or whatever gym-retro version)
-print("Dir retro:", dir(retro))
-print("Retro romfile:", retro.get_romfile_system)
-
-
+print("gym:", gym.__version__)    # 0.21.0
+print("retro:", retro.__version__) # 0.8.0
 print(f"Python version: {sys.version}")
-print(f"Retro version: {retro.__version__ if hasattr(retro, '__version__') else 'Unknown'}\n")
 
 print(retro.data.list_games())
 
 
-
-
-
+# Start game environment
 env = retro.make(game='StreetFighterIISpecialChampionEdition-Genesis')
 
 
+# Closes game environment since we can only run one at a time
+# env.close()
+
+# Test to see everything working
+
+# Reset game to starting state
 obs = env.reset()
-print("Observation shape:", getattr(obs, "shape", obs))
 
-step = 0
+# Flag to false
+done = False
 
-while step < 200:
-    time.sleep(0.1)  # Slow down for visibility
-    env.render()
-    step += 1
+# We only play one game
+for game in range(1):
 
-env.close()
+    # If game is not over.
+    while not done:
+        if done:
+            # We reset the game
+            obs = env.reset()
+
+        # Render environment
+        env.render()
+
+        # We take random actions inside the environment
+        obs, reward, done, info = env.step(env.action_space.sample())
+
+        # We slow down the renders so they are watchable
+        time.sleep(0)
+
+        # We print the reward
+        print(reward)
